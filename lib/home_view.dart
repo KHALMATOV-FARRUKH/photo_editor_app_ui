264a98685e26880d38504f380c5c2a3aee7479db
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-10-22 19:10:49 +0500

    The following packages have been added: flutter pub add animations
    Created by:
    app_colors.dart
    chip_widget.dart
    edit_photo_view.dart
    grid_child.dart

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,5 +1,9 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_editor_app_ui/chip_widget.dart';
+import 'package:photo_editor_app_ui/edit_photo_view.dart';
+import 'package:photo_editor_app_ui/grid_child.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -33,10 +37,21 @@ class HomeView extends StatelessWidget {
               .copyWith(fontWeight: FontWeight.w600),
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {},
-        icon: const Icon(Icons.camera_alt),
-        label: const Text('Take a photo'),
+      floatingActionButton: OpenContainer(
+        transitionDuration: const Duration(milliseconds: 400),
+        middleColor: Theme.of(context).scaffoldBackgroundColor,
+        transitionType: ContainerTransitionType.fadeThrough,
+        closedColor: Colors.transparent,
+        closedElevation: 5,
+        openBuilder: (context, _) => const EditPhotoView(),
+        closedBuilder: (context, openContainer) =>
+            FloatingActionButton.extended(
+          onPressed: () {
+            openContainer();
+          },
+          icon: const Icon(Icons.camera_alt),
+          label: const Text('Take a photo'),
+        ),
       ),
       bottomNavigationBar: NavigationBar(
         destinations: const [
@@ -47,44 +62,131 @@ class HomeView extends StatelessWidget {
         ],
       ),
       body: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          color: Theme.of(context).backgroundColor,
-        ),
-        child: ListView(
-          physics: const ClampingScrollPhysics(),
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Row(
-                children: [
-                  Text(
-                    'My Album',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(fontWeight: FontWeight.w600),
-                  ),
-                  const Spacer(),
-                  Chip(
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                    label: Text(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Theme.of(context).backgroundColor,
+          ),
+          child: ListView(
+            physics: const ClampingScrollPhysics(),
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      'My Album',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.w600),
+                    ),
+                    const Spacer(),
+                    ChipWidget(
+                      label: Text(
+                        'See All',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: GridView.count(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.75,
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 25,
+                  children: const [
+                    GridChild(
+                      count: 132,
+                      description: 'Vacation',
+                      imagePath: 'assets/img/a_1.jpg',
+                    ),
+                    GridChild(
+                      count: 42,
+                      description: 'Building',
+                      imagePath: 'assets/img/a_2.jpg',
+                    ),
+                    GridChild(
+                      count: 72,
+                      description: 'Vacation',
+                      imagePath: 'assets/img/a_3.jpg',
+                    ),
+                    GridChild(
+                      count: 21,
+                      description: 'Geometric',
+                      imagePath: 'assets/img/a_4.jpg',
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  children: [
+                    Text(
+                      'People',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleLarge!
+                          .copyWith(fontWeight: FontWeight.w600),
+                    ),
+                    const Spacer(),
+                    ChipWidget(
+                        label: Text(
                       'See All',
                       style: Theme.of(context)
                           .textTheme
-                          .bodyMedium!
+                          .titleMedium!
                           .copyWith(fontWeight: FontWeight.w600),
-                    ),
-                  )
-                ],
+                    )),
+                  ],
+                ),
               ),
-            )
-          ],
-        ),
-      ),
+              const SizedBox(
+                height: 16,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.18,
+                child: ListView.builder(
+                  itemCount: 4,
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => Padding(
+                    padding:
+                        EdgeInsets.only(left: index == 0 ? 16 : 0, right: 16),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(30),
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image:
+                                  AssetImage('assets/img/p_${index + 1}.jpg'))),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+            ],
+          )),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:photo_editor_app_ui/app_colors.dart';
 import 'package:photo_editor_app_ui/home_view.dart';
 
 void main() {
@@ -9,18 +10,29 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.montserrat().fontFamily,
-        useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      home: HomeView(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            textTheme: TextTheme(
+              bodySmall: TextStyle(color: AppColors.white),
+            ),
+            fontFamily: GoogleFonts.montserrat().fontFamily,
+            useMaterial3: true,
+            appBarTheme: AppBarTheme(
+              backgroundColor: AppColors.lightBlue,
+            ),
+            navigationBarTheme: NavigationBarThemeData(
+              indicatorColor: AppColors.darkBlue,
+            ),
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: AppColors.darkBlue,
+            ),
+            chipTheme: ChipThemeData(backgroundColor: AppColors.cyan),
+            backgroundColor: AppColors.white,
+            scaffoldBackgroundColor: AppColors.lightBlue),
+        home: HomeView());
   }
 }
